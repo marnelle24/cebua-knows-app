d53f696c970cfc3e9243ebde86674d84ea62316e
MarnelleApat <marnelle.apat@bible.org.sg>
2025-06-18 16:03:55 +0800

    first commit and created header and map component

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+// import { RouterLink } from 'vue-router'
+import HeaderDisplay from './components/HeaderDisplay.vue'
+import MapDisplay from './components/MapDisplay.vue'
+</script>
+
+<template>
+  <!-- <div class="headerContainer"> -->
+  <main class="mainWrapper">
+    <HeaderDisplay msg="Everything you need to know about Cebu" />
+    <div class="contentWrapper">
+      <RouterView />
+    </div>
+  </main>
+
+  <MapDisplay />
+
+</template>
+
+<style scoped>
+.mainWrapper {
+  display: flex;
+  row-gap: 25px;
+  flex-direction: column;
+  align-items: center;
+  max-width: 100%;
+  margin: 50px auto;
+}
+
+.contentWrapper {
+  max-width: 100%;
+  background-color: #f8f9fa;
+  opacity: 0.5;
+}
+</style>
---
+++ src/components/HeaderDisplay.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <header class="headerContainer">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div>
+      <h1 class="text-red-400">Cebua<strong class="text-blue-400">Knows</strong></h1>
+      <h3>{{ msg }}</h3>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.headerContainer {
+  display: flex;
+  flex-direction: row;
+  align-items: start;
+  max-width: 100%;
+
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  line-height: 2.5rem;
+  /* position: relative; */
+  /* top: -10px; */
+}
+
+strong {
+  font-weight: 600;
+  color: #FFF;
+}
+
+h3 {
+  font-size: 1rem;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto;
+  width: 4rem;
+}
+
+/* .greetings h1,
+.greetings h3 {
+  text-align: center;
+} */
+
+/* @media (min-width: 1024px) {
+
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+} */
+</style>
