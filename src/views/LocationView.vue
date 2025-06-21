eaba3e5730f995d64567eb2d4f3f3a661cc4bf10
MarnelleApat <marnelle.apat@bible.org.sg>
2025-06-22 07:33:38 +0800

    optimize code and add loader on gpt prompt api integration

--- src/views/LocationView.vue
+++ src/views/LocationView.vue
@@ -93,15 +93,38 @@ async function executeChartGPTPrompt(selectedLocation) {
         </li>
       </ul>
     </div>
-    <div v-else class="text-red-500">
-      Gathering tourist spots for <span>{{ prettyLocation }}</span>...
-      <p v-if="location" class="italic">Please wait...</p>
-    </div>
+    <p v-else class="spinner">
+      <svg version="1.1" id="L4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+        y="0px" viewBox="0 40 60 16" enable-background="new 0 0 0 0" xml:space="preserve">
+        <circle fill="currentColor" stroke="none" cx="6" cy="50" r="6">
+          <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.1" />
+        </circle>
+        <circle fill="currentColor" stroke="none" cx="26" cy="50" r="6">
+          <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.2" />
+        </circle>
+        <circle fill="currentColor" stroke="none" cx="46" cy="50" r="6">
+          <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.3" />
+        </circle>
+      </svg>
+    </p>
   </div>
 </template>
 
 
 <style scoped>
+.spinner {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.spinner svg {
+  width: 50px;
+  height: 50px;
+  display: block;
+  margin: 0 auto;
+}
+
 .numbering {
   font-size: 1.5rem;
   font-weight: bold;
